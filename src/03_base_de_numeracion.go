72cb72991d1e6db4fe081b1c24f9eaa39449ed9e
Sebastian009w <ia3266595@gmail.com>
2023-03-18 10:59:41 -0500

    base de numeraciones

---
+++ src/03_base_de_numeracion.go
@@ -0,0 +1,13 @@
+
+
+/* Base */   /* prefijo*/ /*Description*/										/*Ejemplo*/
+/* binario */   0b				/* Cada digito puede ser 0 o 1*/		0b10001101
+/* octal */   0				/* Cada digito ´puede tomar valores del													al 7*/															012072
+/* Hexadecimal */   0x				/* Cada digito puede tomar valores
+																	0 al 9, y de la A a la F
+																	 representando
+																	los valores del 10 al 15*/	0xAF32
+
+
+produvtoInteriorBruto := 1_419_000_000_000																	
+bitsAgrupados := 0b_1000_1001_0110																	

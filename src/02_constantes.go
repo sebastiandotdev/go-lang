f7b8443a82f3c45db4b79756e33e02580597cb24
Sebastian009w <ia3266595@gmail.com>
2023-03-18 10:51:13 -0500

    constantes

---
+++ src/02_constantes.go
@@ -0,0 +1,22 @@
+
+/* constantes */
+
+/* sintaxis */
+// const <name> = <valor>
+
+
+/* primera forma */
+const Pi = 3.1416
+
+/* segunda forma */
+const Pi float64 = 3.1416
+
+/* Agrupacion de constantes */
+
+const (
+	TipoFunte = "Times new roman"
+	AlturaFuente = 12
+	Subrayado = false
+	Negrita = true
+)
+

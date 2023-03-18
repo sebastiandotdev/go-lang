f4b702b5b267c4c0a05cf466a9e53fb87f43c4cd
Sebastian009w <ia3266595@gmail.com>
2023-03-18 11:12:04 -0500

    operadores

---
+++ src/04_operadores_numericos.go
@@ -0,0 +1,44 @@
+
+/* result: 12 */
+a := 8 + 3 * (1 + 3) % 5
+
+
+/* incremento o decremento */
+b := 20
+c := 30
+
+b++
+c--
+
+/*operadores numericos de comparacion */
+
+/*siempre  el resultaado sera un booleano */
+9 == 8
+9 != 9
+
+6 < 6
+5 <= 2
+
+7 > 4
+8 >= 1
+
+
+/* operador apicable al tipo string */
+
+o := "el cocherito"
+v := "lere"
+
+concat := o + " " + v
+
+
+/* operadores logicos **/
+
+4 > 4 && 4 < 4
+
+4 == 4 || 4 <= 4
+
+!(4 == 4)
+
+/* operadors logicos con binarios */
+
+0b_1010 & 0b_1100 == 0b_1000

f61806bdc381bc7ed87e66532cc9877d1e8cf1f7
Sebastian009w <ia3266595@gmail.com>
2023-03-20 20:06:18 -0500

    if else --ini

---
+++ src/07_if_else.go
@@ -0,0 +1,33 @@
+/*Condicionales*/
+
+/* 
+if <definicion de variable> ; <condicion> {
+	
+}
+*/
+num := 20
+name := "Sebastian"
+
+/* func main() {
+	if valor := rand.int(); valor % 2 == 0 {
+		fmt.Println("El numero, ", valor, "es par")
+	} else {
+		fmt.Println("el numero, ", valor, "es impar")
+	}
+
+	fmt.Println("Adios !")
+}
+*/
+
+
+if num == 20 {
+	fmt.Println("El numero es igual a 20")
+} else {
+	fmt.Println("El numero no es igual a 20")
+}
+
+if name != "sebastian" {
+	fmt.Println("no eres sebastian")
+} else {
+	fmt.Println("bienvenido sebastian")
+}

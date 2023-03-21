7b663e48e25d45e0f625b6358b3f749b4d474e13
Sebastian009w <ia3266595@gmail.com>
2023-03-20 19:56:36 -0500

    se aprende los verbos --ini

---
+++ src/06_verbos.go
@@ -0,0 +1,12 @@
+import "fmt"
+
+/** verbos */
+
+// %v -> Imprime el valor por defecto.
+// %T -> 	Imprime el tipo de dato.
+// %s -> cadena de texto.
+
+
+num := 30
+
+fmt.Println("el tipo de dato de la variable num es: ",%v)
